d05e85fcd611472e9d883ab857019a8f6e518954
Andrea Freeman <afreeman5566@gmail.com>
2021-10-25 16:23:52 -0500

    Homepage Component added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import HomePage from './HomePage';
 
 function App() {
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,13 +3,18 @@
 function HomePage(){
 
     return (
-        <>
+        <div>
 
-        
+        <div>
+            <h1> Title </h1>
+            <p> Summary </p>
+        </div>
+
+        <button> Let's Go </button>
 
 
 
-        </>
+        </div>
     )
 
 }
