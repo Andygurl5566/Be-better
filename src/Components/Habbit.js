c99fec1e16c38270d5f1d68c67a3acb731b5951f
Todd Krieger <twkriege@mtu.edu>
2021-10-26 12:10:08 -0400

    added space for emoji/picture on habbit card

--- src/Components/Habbit.js
+++ src/Components/Habbit.js
@@ -6,6 +6,7 @@ function Habbit () {
     return (
         <div className = "Habbit">
             <h3>This is a Div!</h3>
+            <h1>📖</h1>
             <button>TestButton!</button>
         </div>
     )
