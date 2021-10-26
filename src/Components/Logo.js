94f801faed1dc6d9f0d5051583ed4e21134611ee
Andrea Freeman <afreeman5566@gmail.com>
2021-10-25 20:04:13 -0500

    second page added and other tweaks

---
+++ src/Components/Logo.js
@@ -0,0 +1,11 @@
+// A button that can be clicked to bring you back to the main page
+
+function Logo(){
+    return(
+        <>
+        <h1> LOGO WILL GO HERE </h1>
+        </>
+    )
+}
+
+export default Logo
