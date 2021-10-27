cea3e4b7f7d4a97dd468bc36fc1c623aced129e4
Andrea Freeman <afreeman5566@gmail.com>
2021-10-27 11:08:51 -0500

    About page done just need styling

---
+++ src/Components/Info.js
@@ -0,0 +1,23 @@
+function Info(){
+    return(
+        <>
+            <p>
+                Hobit Habit builds off of the concepts detailed in the book "Atomic Habits". 
+                 All habit result in a response. Positive responses are knowledge, relationships, productivity 
+                and education. Negative responses are stress, negative self talk, etc. In order to increase positive
+                responses, use Hobit Habits to identify and keep track of habits you'd like to change.
+            </p>
+            <ul>
+                <p> Improve your odds by doing the following: </p>
+                <li> 1: Keep Score - identifying our overall day structure and see what habits can be improve. </li>
+                <li> 2: Set a time and place for goals </li>
+                <li> 3: Habit stack - Stack accomplished habits tackling a new habit.  </li>
+                <li> 4: Environment - Create an environment that makes it easier to achieve your goals  </li>
+                <li> 5: Make it attractive - Make our goals attractive so that we can look forward to building your habit.`  </li>
+            </ul>
+        </>
+    )
+
+}
+
+export default Info
