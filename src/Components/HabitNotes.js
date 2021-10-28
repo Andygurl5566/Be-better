9dd75b14123e67f377a0b14354eb99021a7e7bdc
Todd Krieger <twkriege@mtu.edu>
2021-10-28 10:02:32 -0400

    can now edit notes for chosen habits

--- src/Components/HabitNotes.js
+++ src/Components/HabitNotes.js
@@ -1,10 +1,28 @@
+import {useState} from 'react'
+
 
 function HabitNotes({displayHabit}){
+
+    const [noteText, setNoteText] = useState(displayHabit.notes)
+    const [disabled, setDisabled] =useState(true)
+    
+    const handleChange = (e) => {
+        let temp = e.target.value
+        setNoteText(temp)
+    }
+
+    const handleEdit = () => {
+        setDisabled(!disabled)
+        setNoteText(displayHabit.notes)
+    }
+
     return(
         <div>
             <h3 className ="containerTitle">{displayHabit.name}</h3> 
                 <div className = "notes" >
-                <textarea rows="4" cols="50" value={displayHabit.notes}></textarea>
+                <textarea disabled={disabled} onChange={handleChange} rows="6" cols="50" value={disabled? displayHabit.notes : noteText}></textarea>
+                <button onClick={handleEdit}>Edit Notes</button>
+                <button>Save Notes</button>
                 </div>
         </div>
     )
