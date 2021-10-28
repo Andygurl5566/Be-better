4d0a2a0111deabc9a71c4b8613ebea55063ca593
Andrea Freeman <afreeman5566@gmail.com>
2021-10-28 10:48:32 -0500

    resized habits on notes page and set all intial habits to false in db.json

--- src/Components/ChosenHabits.js
+++ src/Components/ChosenHabits.js
@@ -11,7 +11,7 @@ function ChosenHabits({chosenHabit, setDisplayHabit}){
             <h3 className ="containerTitle">{chosenHabit.name}</h3> 
             <div className = "Habbit">
                 <h3>{chosenHabit.notes}</h3>
-                <img src = {chosenHabit.image} alt = {chosenHabit.name}/>
+                <img src = {chosenHabit.image} width="300px" alt = {chosenHabit.name}/>
                 <button onClick={handleDetail}>Details</button>
             </div>
         </div>
